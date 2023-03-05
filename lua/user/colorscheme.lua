ed77fbd4b6ecab80c2fabe3bfad1e202396413f2
Neil <neil@192.168.18.101>
2023-03-06 00:16:23 +0800

    change fillchars and colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,10 +1,13 @@
 require("tokyonight").setup({
 	on_highlights = function(hl, colors)
 		hl.CursorLineNr = {
-			-- fg = colors.blue2,
 			fg = "#d0ff14",
 		}
 	end,
+
+  on_colors = function(colors)
+    colors.border = "#652020"
+  end,
 })
 
 lvim.colorscheme = "tokyonight-night"
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -4,3 +4,12 @@ vim.opt.cursorcolumn = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.virtualedit = "block"
+vim.opt.fillchars = {
+	horiz = "═",
+	vert = "║",
+	horizup = "╩",
+	horizdown = "╦",
+	vertright = " ",
+	vertleft = " ",
+	verthoriz = "╬",
+}
