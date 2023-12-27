ed10a9733f5c53423f297a03118e06f3a6e63ef8
Neil <wrz750726@gmail.com>
2023-12-27 18:56:05 +0800

    feat: add and setup new plugin "copilot-lualine"

--- lua/user/statusline.lua
+++ lua/user/statusline.lua
@@ -3,15 +3,79 @@ lvim.builtin.lualine.options.minumu_width = 40
 
 local components = require("lvim.core.lualine.components")
 
-local lsp = components.lsp
-lsp.cond = nil
+local compo_lsp = {
+	function()
+		local buf_clients = vim.lsp.get_active_clients({ bufnr = 0 })
+		if #buf_clients == 0 then
+			return "%#DiagnosticWarn#" .. " " .. ":[" .. "%*" .. "LSP Inactive" .. "%#DiagnosticWarn#" .. "]" .. "%*"
+		end
 
-lvim.builtin.lualine.sections.lualine_x = {
-  -- components.lsp,
-  lsp,
-  components.treesitter,
-  components.filetype,
-  components.spaces
+		local buf_ft = vim.bo.filetype
+		local buf_client_names = {}
+
+		-- add client
+		for _, client in pairs(buf_clients) do
+			if client.name ~= "null-ls" and client.name ~= "copilot" then
+				table.insert(buf_client_names, client.name)
+			end
+		end
+
+		-- add formatter
+		local formatters = require("lvim.lsp.null-ls.formatters")
+		local supported_formatters = formatters.list_registered(buf_ft)
+		vim.list_extend(buf_client_names, supported_formatters)
+
+		-- add linter
+		local linters = require("lvim.lsp.null-ls.linters")
+		local supported_linters = linters.list_registered(buf_ft)
+		vim.list_extend(buf_client_names, supported_linters)
+
+		local unique_client_names = vim.fn.uniq(buf_client_names)
+
+		local language_servers = "%#DiagnosticInfo#"
+			.. " "
+			.. ":["
+			.. "%*"
+			.. table.concat(unique_client_names, ", ")
+			.. "%#DiagnosticInfo#"
+			.. "]"
+			.. "%*"
+
+		return language_servers
+	end,
+	color = { gui = "bold" },
+	cond = nil,
 }
 
--- lvim.builtin.lualine.options.minumu_width = 40
+local compo_copilot = {
+	"copilot",
+	symbols = {
+		status = {
+			icons = {
+				enabled = "",
+				disabled = "",
+				warning = "",
+				unknown = "",
+			},
+			hl = {
+				enabled = "#6cc644",
+				disabled = "#6272A4",
+				warning = "#ffc777",
+				unknown = "#c53b53",
+			},
+		},
+		spinners = require("copilot-lualine.spinners").dots_negative,
+		spinner_color = "#6272A4",
+	},
+	show_colors = true,
+	show_loading = true,
+}
+
+lvim.builtin.lualine.sections.lualine_x = {
+	-- components.lsp,
+	compo_copilot,
+	components.treesitter,
+	components.spaces,
+	components.filetype,
+	compo_lsp,
+}
