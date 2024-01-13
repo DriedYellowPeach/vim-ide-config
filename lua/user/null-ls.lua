682fa98e55761f6a1f13c64ccf61fe4c22d88365
Neil <wrz750726@gmail.com>
2024-01-13 21:37:24 +0800

    [bugfix]: fix c format style

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -1,6 +1,6 @@
 -- format on save
 lvim.format_on_save.enabled = true
-lvim.format_on_save.pattern = { "*.rs", "*.go", "*.html", "*.java", "*.lua", "*.md", "*.py" }
+lvim.format_on_save.pattern = { "*.rs", "*.go", "*.html", "*.java", "*.lua", "*.md", "*.py", "*.c", "*.cpp" }
 
 -- formatters setup
 local formatters = require("lvim.lsp.null-ls.formatters")
@@ -8,7 +8,12 @@ formatters.setup({
 	{ command = "prettier", filetypes = { "html", "css", "markdown" } },
 	{ command = "rustfmt", filetypes = { "rust" }, args = { "--edition=2021" } },
 	{ command = "google_java_format", filetypes = { "java" } },
-	{ command = "clang-format", filetypes = { "c", "cpp" }, args = { "--style=file" } },
+	{
+		command = "clang-format",
+		filetypes = { "c", "cpp" },
+		-- args = { "-style=file" },
+		extra_args = { "--style={IndentWidth: 4}" },
+	},
 	{ command = "goimports", filetypes = { "go" } },
 	{ command = "gofumpt", filetypes = { "go" } },
 	{ command = "black", filetypes = { "python" } },
