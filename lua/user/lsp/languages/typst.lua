2e233c081bc3694f9c4a18c16e3ce7939ee110a4
Neil <wrz750726@gmail.com>
2024-05-15 10:33:07 +0800

    [feat]: add support for typst

--- lua/user/lsp/languages/typst.lua
+++ lua/user/lsp/languages/typst.lua
@@ -1,5 +1,7 @@
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "typst-lsp" })
 vim.filetype.add({ extension = { typ = "typst" } })
+
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "typst-lsp" })
+
 local lsp_manager = require("lvim.lsp.manager")
 lsp_manager.setup("typst_lsp", {
 	settings = {
